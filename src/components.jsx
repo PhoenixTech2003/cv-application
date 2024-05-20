891aefa44342c4d9f0a464848abd21939d1ab4ef
Chiyembekezo <phoenixtech2003@gmail.com>
2024-05-20 19:24:02 +0200

    Add conditiional render for form sections

--- src/components.jsx
+++ src/components.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react"
+
+function Form({inputs,nextForm,prevForm }){
+    return(
+        <>
+        
+        {inputs.map(input =>{
+        
+          return(
+  
+              <div key={input.name[0]} className = 'form-content'>
+                  <label htmlFor={input.name[0]}>{input.name[1]}</label>
+                  <input type={input.name[2]} id={input.name[0]} />
+                  
+  
+              </div>
+  
+  
+          )
+      })}
+      <button  onClick={nextForm}>Next</button>
+      <button  onClick={prevForm}>Prev</button>
+
+      </>
+        
+
+    )
+
+    
+
+}
+
 function SideBar({children}){
 
     return(
@@ -10,30 +42,101 @@ function SideBar({children}){
             </div>
             <form action="">
             <h4>GENERAL INFORMATION</h4>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-
-            <label htmlFor="phone">Phone Number</label>
-            <input type="text" id="phone"/>
-
+            {children}
 
             </form>
-            {children}
+        
         </section>
     )
 
 }
 
-function MainDisplay(){
+function cvDisplay(){
     return (
         <section className="cv-section">
 
         </section>
     )
 }
+function Main(){
+    const [formNumber, setNumber] = useState(0)
+
+    function viewNextForm(e){
+        e.preventDefault();
+        setNumber(formNumber + 1);
+    }
+
+    function viewPrevForm(e){
+        e.preventDefault();
+        setNumber(formNumber - 1);
+    }
+
+    if (formNumber === 0){
+        return(
+    
+            <SideBar>
+                <Form inputs={[
+                    {name:["name","Name", "text"]},
+                    {name:["email","Email", "Email"]},
+                    {name:["phone","Phone"]},
+
+                ]}
+                
+                nextForm={viewNextForm}
+                prevForm={viewPrevForm}
+                
+                />
+            </SideBar>
+                
+            
+        )
 
 
-export  {SideBar, MainDisplay};
+
+    }else if(formNumber === 1){
+
+        return (
+    
+            <SideBar>
+                <Form inputs={[
+                    {name:["schoolname","School"]},
+                    {name:["title","Title Of Study"]},
+                    {name:["date", "Date", "date"]}
+                    
+                ]
+                    
+                    
+                    }
+                    nextForm={viewNextForm}
+                    prevForm={viewPrevForm}/>
+            </SideBar>
+                
+            
+        )
+    }else if(formNumber === 2){
+
+        return (
+    
+            <SideBar>
+                <Form inputs={[
+                    {name:["companyName","Company Name", "text"]},
+                    {name:["postition","Position", "text"]},
+                    {name:["responsibilities", "Responsiblilities", "textarea"]},
+                    {name:["startDate", "Start Date", "date"]},
+                    {name:["endDate", "End Date", "date"]},
+
+                    
+                ]
+                    
+                    
+                    }
+                    prevForm={viewPrevForm}/>
+            </SideBar>
+                
+            
+        )
+    }
+} 
+
+export  {Main};
